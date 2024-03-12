9994b2ffe0b2296324d3b63d0d1054ea217d50b8
Bhavnesh Arora <erbhavnesharora@gmail.com>
2024-03-13 01:26:27 +0530

    added

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -61,9 +61,9 @@ courseRouter.post("/", async (req, res) => {
         error: true,
       });
 
-    req.body.forEach((el) => {
-      el.adminId = "admin" + decoded.userId;
-    });
+    // req.body.forEach((el) => {
+    //   el.adminId = "admin" + decoded.userId;
+    // });
     try {
       await CourseModel.insertMany(req.body);
       res.send({
